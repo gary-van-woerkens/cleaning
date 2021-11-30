50d35ab0090d7d411cddd00b6f34df9b45558e1b
gary-van-woerkens <gary.van-woerkens@sg.social.gouv.fr>
2021-11-30 23:10:01 +0100

    Update tests

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,15 +2,6 @@ import * as process from "process"
 import * as cp from "child_process"
 import * as path from "path"
 import { test } from "@jest/globals"
-// import { expect, test } from "@jest/globals"
-// import purge from "../src/purge"
-
-// const container = "fabrique/standup"
-
-// test(`Purge container ${container}`, async () => {
-//   const count = await purge(container, 1, 100, 4)
-//   expect(count).toBe(0)
-// })
 
 test("test runs", () => {
   process.env["INPUT_RETENTION-WEEKS"] = "4"
